f9b3d9306ec7a9b743c963b124efd00c3d93fc4d
dajung <dajung_ju@hancom.com>
2022-05-31 15:21:26 +0900

    views 생성 -> url과의 연동까지 완료

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # polls안에서의 path가 없다는 뜻
+    # polls안에서의  path가 없다는 뜻
+    # /polls/
     path('', views.index, name='index'),
+    # /polls/5, views.detail이라는 함수 수행
+    path('<int:question_id>/', views.detail, name='detail'),
+    # /polls/5/results
+    path('<int:question_id>/results/', views.results, name='results'),
+    # /polls/5/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
