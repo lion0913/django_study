776211b1983eaedaf164db7f896a387478c77157
dajung <dajung_ju@hancom.com>
2022-05-31 15:04:24 +0900

    관리자 사이트에서의 테이블을 관리할 수 있도록 보여주는 화면 추가

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Question, Choice
 
 # Register your models here.
+
+# 관리자 사이트에서 관리하고자 하는 테이블 추가(화면에 뿌려줌 -> CRUD 가능)
+admin.site.register(Question)
+admin.site.register(Choice)
--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # polls안의 path가 없다는 뜻
+    # polls안에서의 path가 없다는 뜻
     path('', views.index, name='index'),
 ]
